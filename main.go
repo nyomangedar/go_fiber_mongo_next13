a79d264656349bee517b05da98f6c86b3377f520
nyomangedar <nyoman.gedar@gmail.com>
2023-07-31 15:42:24 +0100

    initial push

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fiber-mongo-api/configs"
+	"fiber-mongo-api/routes"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{"data": "Hello from FIber & mongo"})
+	})
+	//run database
+	configs.ConnectDB()
+
+	//routes
+	routes.ArticleRoutes(app)
+
+	app.Listen(":6000")
+}
